18c5b8ab4efbe118c7d2a55998b9ee1f563b071f
HoonJae <rlagnswo0505@naver.com>
2022-07-15 16:41:55 +0900

    social_login create

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import store from './store';
+import router from './router';
 
-createApp(App).mount('#app')
+createApp(App).use(router).use(store).mount('#app');
+
+window.Kakao.init('22c7aaf0e5a912c4bc0bbb5115a03b43');
