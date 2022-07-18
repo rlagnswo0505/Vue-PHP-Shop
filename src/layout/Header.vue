b1664e04d23d124ad9818975b9fcacd2aa0bfc7d
HoonJae <rlagnswo0505@naver.com>
2022-07-18 14:55:57 +0900

    Header, Footer create

---
+++ src/layout/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a href="#" class="navbar-brand"> soldout </a>
+      <button class="navbar-brand" type="button" data-bs-targe="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Tooggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'header',
+};
+</script>
+
+<style></style>
